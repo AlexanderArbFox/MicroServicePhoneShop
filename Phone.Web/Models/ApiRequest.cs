b0a963c602321dd86faf2d4e3807763699e7856a
www.arbuzfox11 <www.arbuzfox11@mail.ru>
2022-06-08 22:13:53 +0300

    Add class ApiRequest

---
+++ Phone.Web/Models/ApiRequest.cs
@@ -0,0 +1,30 @@
+﻿using static Phone.Web.SD;
+
+namespace Phone.Web.Models
+{
+    /// <summary>
+    /// Model to call API 
+    /// </summary>
+    public class ApiRequest
+    {
+        /// <summary>
+        /// Type request. Default Method Get 
+        /// </summary>
+        public ApiType apiType { get; set; } = ApiType.GET;
+
+        /// <summary>
+        /// the url to which we want to send this api
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// data message in body 
+        /// </summary>
+        public object Data { get; set; }
+
+        /// <summary>
+        /// user authentication
+        /// </summary>
+        public string AccessToken { get; set; }
+    }
+}
