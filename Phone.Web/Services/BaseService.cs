04edfe486f6f43668aedd898c9f2e07e0d602791
www.arbuzfox11 <www.arbuzfox11@mail.ru>
2022-06-08 22:21:59 +0300

    Add interface IBaseService

---
+++ Phone.Web/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using Phone.Web.Models;
+using Phone.Web.Services.IServices;
+
+namespace Phone.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseDto { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Phone.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using Phone.Web.Models;
+
+namespace Phone.Web.Services.IServices
+{
+    public interface IBaseService:IDisposable
+    {
+        /// <summary>
+        /// the response will be received when a request is made to any service
+        /// </summary>
+        ResponseDto responseDto { get; set; }
+
+        /// <summary>
+        /// sending a request. We transmit everything related to apirequest
+        /// </summary>
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
