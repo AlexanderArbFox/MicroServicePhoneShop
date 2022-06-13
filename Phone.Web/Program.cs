5c8bdc122a3023aaaf315a5451a2a82b591b1a59
www.arbuzfox11 <www.arbuzfox11@mail.ru>
2022-06-13 18:48:36 +0300

    add dependencies http client

--- Phone.Web/Program.cs
+++ Phone.Web/Program.cs
@@ -1,6 +1,16 @@
+using Phone.Web;
+using Phone.Web.Services;
+using Phone.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+/// <summary>
+/// creating http dependencies
+/// </summary>
+builder.Services.AddHttpClient<IGoodService, GoodService>();
+SD.GoodAPIBase = builder.Configuration["ServiceUrl:GoodAPI"];
+builder.Services.AddScoped<IGoodService, GoodService>();    
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
