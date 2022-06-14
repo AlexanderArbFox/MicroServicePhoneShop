fb51cd718c4667eafa78492ad0b24168990ccf8d
www.arbuzfox11 <www.arbuzfox11@mail.ru>
2022-06-14 17:23:00 +0300

    add controller a good

---
+++ Phone.Web/Controllers/GoodController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Phone.Web.Models;
+using Phone.Web.Services.IServices;
+
+namespace Phone.Web.Controllers
+{
+    public class GoodController : Controller
+    {
+        private readonly IGoodService _goodService;
+        public GoodController(IGoodService goodService)
+        {
+            _goodService = goodService;
+        }
+        public async Task<IActionResult> GoodIndex()
+        {
+            List<GoodsDto> listGood = new();
+            var response = await _goodService.GetAllGoodsAsync<ResponseDto>();
+            if(response != null && response.IsSuccess)
+            {
+                listGood = JsonConvert.DeserializeObject<List<GoodsDto>>(Convert.ToString(response.Result));
+            }
+            return View(listGood);
+        }
+    }
+}
